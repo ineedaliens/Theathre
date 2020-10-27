7f534a6d6ac40edf5a8e75c818568bd38e736e9c
Evgenij <timlamb146@gmail.com>
2020-10-27 23:03:22 +0500

    Create Main Table View Cell Add to custom Cell: UIImage, UILabel (3) Add tableView.backgroundColor as Launch Screen Color Setting custom cell for Outlets from Main Table View Cell Set tableView.estimatedRowHeight Set cell.backgroundColor Fix images array Add Method Did Select Row At And add deselectRow Method

---
+++ Theathre/MainTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MainTableViewCell.swift
+//  Theathre
+//
+//  Created by Евгений on 27.10.2020.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    @IBOutlet weak var images: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+}
--- Theathre/MainTableViewController.swift
+++ Theathre/MainTableViewController.swift
@@ -15,12 +15,14 @@ class MainTableViewController: UITableViewController {
     let theathreNames = ["Екатеринбургский государственный академический театр оперы и балета","Екатеринбургский музыкально-драматический театр Сцена","Коляда-Театр","Волхонка","Камерный Театр Объединенного Музея Писателей Урала"]
     let type = ["Театр оперы и балета","Музыкально-драматический театр","Частный театр","Драматический театр","Камерный театр"]
     let location = ["просп. Ленина, 46А, Екатеринбург, Свердловская обл., 620075","просп. Ленина, 48 А, Екатеринбург, Свердловская обл., 620100","просп. Ленина, 97, Екатеринбург, Свердловская обл., 620062","ул. Малышева, 21/1, Екатеринбург, Свердловская обл., 620014","ул. Пролетарская, 18, Екатеринбург, Свердловская обл., 620075"]
-    let images = ["Екатеринбургский государственный академический театр оперы и балета","катеринбургский музыкально-драматический театр Сцена","Коляда-Театр","Волхонка","Камерный Театр Объединенного Музея Писателей Урала"]
+    let images = ["Екатеринбургский государственный академический театр оперы и балета","Екатеринбургский музыкально-драматический театр Сцена","Коляда-Театр","Волхонка","Камерный Театр Объединенного Музея Писателей Урала"]
     
     
     // MARK: - METHOD VIEW DID LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.backgroundColor = #colorLiteral(red: 0, green: 0.6672332883, blue: 0.7453075051, alpha: 1)
+        tableView.estimatedRowHeight = 85
     }
 
     
@@ -46,15 +48,26 @@ class MainTableViewController: UITableViewController {
     
     // MARK: - TABLE METHOD CELL FOR ROW AT INDEX PATH
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = theathreNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: images[indexPath.row])
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
-        cell.imageView?.clipsToBounds = true
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MainTableViewCell
+        cell.nameLabel.text = theathreNames[indexPath.row]
+        cell.typeLabel.text = type[indexPath.row]
+        cell.locationLabel.text = location[indexPath.row]
+        cell.images.image = UIImage(named: images[indexPath.row])
+        cell.images.layer.cornerRadius = 29.5
+        cell.images.clipsToBounds = true
 
+        
+        cell.backgroundColor = #colorLiteral(red: 0, green: 0.6672332883, blue: 0.7453075051, alpha: 1)
+        
         return cell
     }
 
+    
+    // MARK: - TABLE METHOD DID SELECT ROW AT INDEX PATH
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     
     // MARK: - PREPARE FOR SEGUE
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
