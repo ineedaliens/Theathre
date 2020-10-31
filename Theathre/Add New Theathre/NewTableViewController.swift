394a2796338a2c3848464b14edc61b19a08a7815
Evgenij <timlamb146@gmail.com>
2020-10-31 16:04:49 +0500

    Add new icon in action sheet Set title action sheet alignment.left

--- Theathre/Add New Theathre/NewTableViewController.swift
+++ Theathre/Add New Theathre/NewTableViewController.swift
@@ -37,13 +37,19 @@ class NewTableViewController: UITableViewController, UIImagePickerControllerDele
     // MARK: - TABLE METHOD DID SELECT ROW AT INDEX PATH
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let photolibraryIcon = #imageLiteral(resourceName: "folder")
             let ac =  UIAlertController(title: nil, message: "Выбери действие", preferredStyle: .actionSheet)
             let camera = UIAlertAction(title: "Камера", style: .default, handler: {_ in
                 self.chooseImagePicker(sourse: .camera)
             })
+            camera.setValue(cameraIcon, forKey: "image") // add icon in action sheet for key
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAligment") // set titleTextAligment.left
             let photolibrary = UIAlertAction(title: "Галерея", style: .default, handler: {_ in
                 self.chooseImagePicker(sourse: .photoLibrary)
             })
+            photolibrary.setValue(photolibraryIcon, forKey: "image") // add icon in action sheet for key
+            photolibrary.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAligment") // set titleTextAligment.left
             let cancel = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
             ac.addAction(camera)
             ac.addAction(photolibrary)
